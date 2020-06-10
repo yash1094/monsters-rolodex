c870d554f263470074094ef8264178a07caf3b82
yoshi <yoshi@yoshis-MacBook-Pro.local>
2020-06-10 13:32:55 -0400

    Added a loading component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
-import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import LoadingSpinner from "./components/loading/loading.component";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
+      isLoading: true,
       monsters: [],
-      searchField: ''
+      searchField: "",
     };
-
-    // this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({ searchField: e.target.value });
   };
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users, isLoading: false }));
   }
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
+    const { monsters, searchField, isLoading } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
-        <SearchBox
-          placeholder="search monster"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters} />
+        {isLoading ? (
+          <LoadingSpinner></LoadingSpinner>
+        ) : (
+          <div>
+            <SearchBox
+              placeholder="search monster"
+              handleChange={this.handleChange}
+            />
+            <CardList monsters={filteredMonsters} />{" "}
+          </div>
+        )}
       </div>
     );
   }
